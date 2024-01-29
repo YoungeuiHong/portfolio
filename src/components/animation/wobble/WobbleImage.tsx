257a6714d428fef15051468f60c190ec3befa948
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-01-29 14:50:06 +0900

    :zap: Fix: 프로필 사진 priority 옵션 값 true로 설정

--- src/components/animation/wobble/WobbleImage.tsx
+++ src/components/animation/wobble/WobbleImage.tsx
@@ -6,6 +6,7 @@ interface WobbleImageProps {
   alt?: string;
   width?: number;
   height?: number;
+  priority?: boolean;
 }
 
 export default function WobbleImage({
@@ -13,13 +14,20 @@ export default function WobbleImage({
   alt = "image",
   width = 300,
   height = 300,
+  priority = false,
 }: WobbleImageProps) {
   const divStyle = { width: width, height: height };
 
   return (
     <div className={styles.container} style={divStyle}>
       <div className={styles.shape} style={divStyle}>
-        <Image src={src} alt={alt} width={width} height={height} />
+        <Image
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+          priority={priority}
+        />
       </div>
     </div>
   );
--- src/sections/introduction/view/Profile.tsx
+++ src/sections/introduction/view/Profile.tsx
@@ -3,7 +3,11 @@ import WobbleImage from "@/components/animation/wobble/WobbleImage";
 export default function Profile() {
   return (
     <div style={{ width: 350, marginLeft: 100 }}>
-      <WobbleImage src={"/assets/image/profile.jpg"} alt={"profile"} />
+      <WobbleImage
+        src={"/assets/image/profile.jpg"}
+        alt={"profile"}
+        priority={true}
+      />
     </div>
   );
 }
