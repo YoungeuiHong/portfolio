ec3a2898118792895d2f904a99f8240b9c278b71
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-01-31 13:46:46 +0900

    :lipstick: Style: SectionLayout padding 변경

--- src/sections/layout.tsx
+++ src/sections/layout.tsx
@@ -1,34 +1,34 @@
 import { ReactNode } from "react";
-import { Stack, Typography } from "@mui/material";
+import { Stack, Theme, Typography } from "@mui/material";
 import FadeInContainer from "@/components/animation/fadein/FadeInContainer";
+import { SxProps } from "@mui/system";
 
 interface SectionLayoutProps {
   id: string;
   title: string;
   children: ReactNode;
+  stackSx?: SxProps<Theme>;
 }
 
 export default function SectionLayout({
   id,
   title,
   children,
+  stackSx,
 }: SectionLayoutProps) {
   return (
     <FadeInContainer>
       <Stack
         alignItems={"center"}
         sx={{
-          marginTop: 0,
+          py: 10,
           width: "100%",
+          ...stackSx,
         }}
         spacing={2}
       >
         <div style={{ textAlign: "center" }}>
-          <Typography
-            id={id}
-            variant={"h4"}
-            sx={{ fontWeight: 700, paddingTop: 10 }}
-          >
+          <Typography id={id} variant={"h4"} sx={{ fontWeight: 700 }}>
             {title}
           </Typography>
         </div>
